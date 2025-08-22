9175e01dec6d0c065facf7b39775758522a1ec93
menegonlucas <lucasmenegon14@gmail.com>
2025-08-22 10:31:17 -0300

    corrigidos os diretorios

--- api/server.js
+++ api/server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const routes = require('./src/routes');
+const routes = require('../src/routes');
 
 const port = process.env.PORT || 3001;
 const app = express();
